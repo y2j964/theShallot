8da0c5fa7f224b1f07231e44a18c2bc02921e849
Justin Mooney <y2j964@aol.com>
2019-03-24 23:37:12 -0400

    add aria accessibility

--- src/js/main.js
+++ src/js/main.js
@@ -7,15 +7,16 @@ const lSearchbar = document.querySelector('.l-searchbar');
 const hamburgerMenu = document.querySelector('.js-hamburger-menu');
 const userIcon = document.querySelector('.js-icon-user');
 const body = document.querySelector('body');
+const dropdownNav = document.querySelector('#dropdown-nav');
 const jsModals = document.querySelector('.js-modals');
+const lModals = document.querySelectorAll('.l-modal');
 const jsModalMenu = jsModals.querySelector('.js-modal-menu');
 const jsModalForm = jsModals.querySelector('.js-modal-form');
 const xIcons = document.querySelectorAll('.js-icon-close');
 const kinjaCloseIcon = document.querySelector('.js-icon-close-kinja');
 const tabSwitches = document.querySelector('.tab-switches');
-const [homeSwitch, userSwitch] = tabSwitches.querySelectorAll('.js-home-switch');
-// const userSwitch = tabSwitches.querySelector('.js-user-switch');
-// const tabUser = tabSwitches.querySelector('.js-icon-user');
+const [homeSwitch, userSwitch] = tabSwitches.querySelectorAll('.js-tab-switch');
+const newsletterForm = document.querySelector('.newsletter__form');
 const login = document.querySelector('.js-login');
 const userName = document.querySelector('#username');
 const userNameError = document.querySelector('#js-username-error');
@@ -29,13 +30,65 @@ const emailRegex = /\S+@\S+\.\S+/;
 
 // FUNCTIONS //
 
+function ariaExpanded(e) {
+  // set up as conditional so that it can be used for both mouseover and mouseout
+  console.log(e.currentTarget);
+  console.log(e.target);
+  if (!e.currentTarget === e.target) {
+    return
+  }   
+  e.currentTarget.getAttribute('aria-expanded') === 'false'
+    ? e.currentTarget.setAttribute('aria-expanded', 'true') 
+    : e.currentTarget.setAttribute('aria-expanded', 'false') 
+  }  
+
+function updateAriaCheckbox(e) {  
+  if (e.target.classList.contains('checkbox')) {    
+    e.target.getAttribute('aria-checked') === 'true'
+      ? e.target.setAttribute('aria-checked','false')
+      : e.target.setAttribute('aria-checked','true');
+  }  
+}
+
+function modalTrapFocus() {
+  // trap focus inside of modal by inerting all other siblings
+  for (let i = 0; i < body.children.length; i++) {    
+    if (body.children[i] !== jsModals) {
+      body.children[i].setAttribute('inert', 'true');
+    }
+  }  
+  // inert the invisible modal
+  for (let n = 0; n < lModals.length; n++) {
+    if (!lModals[n].classList.contains('is-visible')) {
+      lModals[n].setAttribute('inert', 'true');
+    } 
+  }
+  // set all inerts back to default
+  for (let i = 0; i < body.children.length; i++) { 
+    if (body.children[i] !== jsModals) {
+      body.children[i].setAttribute('inert', 'false');
+    }
+  }
+  // set all inerts back to default
+  for (let n = 0; n < lModals.length; n++) {
+    if (!lModals[n].classList.contains('is-visible')) {
+      lModals[n].setAttribute('inert', 'false');
+    } 
+  }
+}
+
 // hamburger-menu expand and collapse
-function expandMenu() {
+function expandModalMenu() {
   // remove scroll from body
   body.classList.toggle('no-scroll');
   // toggle the display of modal
   // the first modal represents the menu we want, so we'll just return the first we find
   jsModals.querySelector('.l-modal').classList.toggle('l-modal--is-visible');
+  const focusElement =   jsModals.querySelector('button');
+  // add a delay so the modal can load before focus is applied
+  setTimeout(function (){
+    focusElement.focus();
+  }, 100);
   tabSwitches.classList.add('tab-switches--is-visible');
   kinjaCloseIcon.classList.add('icon-close--is-hidden');
   // if menu is open and click on user, (1)toggle is-visible between two modals, (
@@ -63,7 +116,7 @@ function validateForm(e) {
 }
 
 // display and remove modal on icon clicks
-function displayModal() {
+function displayModalForm() {
   // remove scroll from body
   if (!body.classList.contains('no-scroll')) {
     body.classList.add('no-scroll');
@@ -75,19 +128,29 @@ function displayModal() {
 
   // display modal
   jsModalForm.classList.add('l-modal--is-visible');
+  jsModalForm.setAttribute('disabled', 'true');
 }
 
-function closeModal(e) {
+function closeModal(e) {  
+
   const lModal = document.querySelector('.l-modal--is-visible');
-
-  if (!e.target.classList.contains('js-icon-close')) {
-    return;
-  }
-
+  
+  // disappear the l-modal and the tabSwitch container
   lModal.classList.remove('l-modal--is-visible');
   tabSwitches.classList.remove('tab-switches--is-visible');
+  
+  // restore regular close icon to Kinja in case accessed through icon-user on home page
   kinjaCloseIcon.classList.remove('icon-close--is-hidden');
+  // restore scroll to body
   body.classList.remove('no-scroll');
+  
+  // revert back to homeSwitch in the event that you close when the userSwitch is active
+  if (!homeSwitch.classList.contains('tab-switches__item--is-active')) {
+    homeSwitch.classList.add('tab-switches__item--is-active');
+    homeSwitch.setAttribute('aria-pressed', 'true');
+    userSwitch.classList.remove('tab-switches__item--is-active');
+    userSwitch.setAttribute('aria-pressed', 'false');
+  }
 }
 
 // clicks on tabSwitches
@@ -99,8 +162,11 @@ function switchModal(e) {
       const activeTab = tabSwitches.querySelector('.tab-switches__item--is-active');
       // remove active state from activeTab
       activeTab.classList.remove('tab-switches__item--is-active');
+      // update aria-pressed
+      activeTab.setAttribute('aria-pressed', 'false');
       // add active background color on newly activated tab
       e.target.classList.add('tab-switches__item--is-active');
+      e.target.setAttribute('aria-pressed', 'true');
       // switch modals
       jsModalMenu.classList.toggle('l-modal--is-visible');
       jsModalForm.classList.toggle('l-modal--is-visible'); 
@@ -112,8 +178,7 @@ function switchModal(e) {
 function expandSearchBar(e) {
   if (!e.target.classList.contains('js-searchbar-toggler')) {
     return;
-  }
-
+  }  
   lSearchbar.classList.toggle('l-searchbar--is-expanded');
 }
 
@@ -158,19 +223,51 @@ function reelTransition(e) {
 // EVENTS //
 
 // clicks on hamburger menu
-hamburgerMenu.addEventListener('click', expandMenu);
+hamburgerMenu.addEventListener('click', expandModalMenu);
 
+// update aria-expanded on dropdown menu
+dropdownNav.addEventListener("mouseenter", ariaExpanded);
+dropdownNav.addEventListener("focusin", ariaExpanded);
+// dropdownNav.addEventListener("focusin", function(e) {
+  // if (e.target === dropdownNav.querySelector('.dropdown__item')) {
+  // ariaExpanded;
+  // }
+// })
+dropdownNav.addEventListener("focusout", ariaExpanded);
+dropdownNav.addEventListener("mouseleave", ariaExpanded);
+
+// clicks on magnifying glass and x
 lSearchbar.addEventListener('click', expandSearchBar);
 
+// clicks on checkbox
+newsletterForm.addEventListener("click", updateAriaCheckbox);
+
 // clicks on user icon
-userIcon.addEventListener('click', displayModal);
+userIcon.addEventListener('click', displayModalForm);
 
 // clicks on close icon
-jsModals.addEventListener('click', closeModal);
+jsModals.addEventListener('click', function(e) {
+  if (!e.target.classList.contains('js-icon-close')) {
+    return;
+  }
+  closeModal(e);
+})
 
+// close modal on esc press
+jsModals.addEventListener('keyup', function(e) {
+  if (e.key === "Escape") {    
+    // console.log("standing in the window");
+    closeModal();
+  }
+})
+
+// clicks on tab-switches
 tabSwitches.addEventListener('click', switchModal);
 
+// mouse over reel article thumbnails
 reelStoryNav.addEventListener('mouseover', reelTransition);
+reelStoryNav.addEventListener('focusin', reelTransition);
+reelStoryNav.addEventListener('focusout', reelTransition);
 
 // submits on Kinja form
 login.addEventListener('submit', validateForm);
